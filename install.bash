b1de5eac355db3e00093ea9b26cc3b33b79cf25d
Daniel Wheeler <daniel.wheeler2@gmail.com>
2016-03-22 18:18:30 -0400

    First attempt at Ansible installation

---
+++ install.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! dpkg -s ansible > /dev/null; then
+    sudo apt-get install -y ansible python-apt
+fi
+
+## be sure to use the system python with ansible at this point
+export PATH=/usr/bin:$PATH
+/usr/bin/ansible-playbook -K install.yaml $@
