df20c8cb86ee43a042c3d11724b208d271879e2d
Daniel Wheeler <daniel.wheeler2@gmail.com>
2016-06-06 11:29:14 -0400

    Change name form corr to corrcli

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='corr',
+    name='corrcli',
     version="0.1",
-    packages=["corr"],
+    packages=["corrcli"],
     include_package_data=True,
     maintainer='Faical Yannick P. Congo',
     description=('The command line client to the CoRR platform.'),
-    url='https://github.com/usnistgov/corr',
+    url='https://github.com/wd15/corr-cli',
     classifiers=[
         "Development Status :: 1 - Alpha",
         "Programming Language :: Python",
     ],
     entry_points={
         'console_scripts': [
-            'corr = corr.main.cli:handle',
+            'corrcli = corrcli.main.cli:handle',
         ],
     },
     test_suite='nose.collector',
